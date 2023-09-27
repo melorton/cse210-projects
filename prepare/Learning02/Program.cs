949b7d487e4ef113161d00b1e3c8447d7450b9c5
melorton <ortonfamily1995@gmail.com>
2023-09-27 16:24:09 -0600

    week 3 lesson

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2021;
+        job1._endYear = 2023;
+       
+        
+        Job job2 = new Job();
+        job2._company = "Google";
+        job2._jobTitle = "Designer";
+        job2._startYear = 2017;
+        job2._endYear = 2023;
+       
+
+        Resume myResume = new Resume();
+        myResume._name = "Melissa Orton";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.Display();
+
+        
     }
 }
